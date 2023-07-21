3fd43c1a2c3e39216ecf194dddaf006d15d5b077
Kibigo <jacksonkibigo99@gmail.com>
2023-07-21 20:28:11 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function(){
-  document.querySelector('form').addEventListener('click', function(e){
-    e.preventDefault();
-  })
-})
+const formDoc = document.getElementById("create-task-form");
+const input = document.getElementById("new-task-description");
 
 
+const list = document.getElementById("tasks");
 
 
+const addListItem = function(e){
+  e.preventDefault();
+  const addedList = document.createElement("li");
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "x";
+  deleteButton.addEventListener('click', function(){
+    
+    addedList.remove();
+    deleteButton.remove();
+  });
+  addedList.textContent = input.value;
+  list.appendChild(addedList);
+  list.appendChild(deleteButton);
+  input.value = "";
+}
+
+formDoc.addEventListener("submit", addListItem);
